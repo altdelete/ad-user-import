bb65865ef67b2ae8dff0530a3e7ae79c53ad4bc5
Benjamin Cole <benjamin.cole@mac.com>
2023-03-21 15:20:32 -0400

    moved execution guide to README.md

--- export.sh
+++ export.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Overview:
-# This script queries an Apple Directory group and exports it's members (including members of nested groups) into a csv template that can be used to create and/or update the DemoKit on-prem Active Directory user objects.
-#
-# Usage
-# Simply replace the values of the variables in the section below with whatever data you want populated into the corresponding csv file headers.
-#
-# The ADGROUP variable expects the unique 'RealName/RecordName' OD group attribute, or simply put: just copy and paste the group name from Apple Directory.
-#
-# For best results, make sure to delete or move the users.csv file out of the script directory before each execution, otherwise it will add the output of the run to the bottom of an existing document.
-
 # Variables
 ADGROUP="DemoKit ENT User Accounts"
 PASSWORD='korpug-6haqzi-ciCj5n'
